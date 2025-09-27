581c90a7d39e880372dea4f488f5369f4ed5778e
Culver22 <o.culver1@newcastle.ac.uk>
2025-09-27 11:34:40 +0100

    Added CLI skeleton with switch cases and Todos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,42 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        if (args.length == 0) { // No Argument, print the help string of acceptable args
+            printHelp();
+            return;
+        }
+
+        switch (args[0]) {
+            case "add" -> { //Todo add prompt for title + entry
+                System.out.println("Adding a new journal entry");
+            }
+            case "remove" -> { //Todo remove entry from file
+                System.out.println("Removing a new journal entry");
+            }
+            case "list" -> { //Todo read from file and print all entries
+                System.out.println("Listing all journal entries");
+            }
+            case "searchKey" -> { // Todo Show entries using a keyword
+                System.out.println("Searching for journal entry by keyword");
+            }
+            case "searchDate" -> { //Todo Show entries using a Date
+                System.out.println("Searching for journal entry by date");
+            }
+            case "searchID" -> { // Todo Show entries using an ID
+                System.out.println("Searching for journal entry by ID");
+            }
+            default -> printHelp();
+        }
+    }
+
+    private static void printHelp() {
+        System.out.println("""
+                CLI-Journal Commands:
+                    add        - create a new journal entry
+                    remove     - remove an entry from the file
+                    list       - list all journal entries
+                    searchKey  - search for entries containing a keyword
+                    searchDate - search for entries created on a date
+                    searchID   - search for a specific entry by its ID
+                """);
     }
 }
