4bc68f63ed7b71e6921afbba4fa47eafe9041091
Culver22 <o.culver1@newcastle.ac.uk>
2025-09-28 19:08:10 +0100

    Created the Constructor and attributes for the JournalStorage class.

--- src/main/java/JournalStorage.java
+++ src/main/java/JournalStorage.java
@@ -1,2 +1,19 @@
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class JournalStorage {
+    private static final String EntrySeparator = "---";
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+
+    private final Path file;
+
+    public JournalStorage(Path file) {
+        this.file = file;
+    }
 }
