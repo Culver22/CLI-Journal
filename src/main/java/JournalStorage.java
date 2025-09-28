e3026c0e5fdb416f19cd587b7a9b205e449b76c0
Culver22 <o.culver1@newcastle.ac.uk>
2025-09-28 20:21:29 +0100

    Added the beginnings of LoadAll (Journal entries). This function collects lines from journal entries and adds them to a temporary list as a string - each entry is separated by a --- and so each entry is stored as a block string in the list, where it will then be parsed in parseBlock()

--- src/main/java/JournalStorage.java
+++ src/main/java/JournalStorage.java
@@ -16,4 +16,37 @@ public class JournalStorage {
     public JournalStorage(Path file) {
         this.file = file;
     }
+
+    public List<JournalEntry> loadAll() {
+        List<JournalEntry> journalEntries = new ArrayList<>();
+        if (!Files.exists(file)) {
+            // return empty list if file doesn't exist
+            return journalEntries;
+        }
+
+        try {
+            List<String> lines = Files.readAllLines(file, StandardCharsets.UTF_8);
+
+            List<String> blockLines = new ArrayList<>();
+
+            for (String line : lines) {
+                if (line.startsWith(EntrySeparator)) { // end of block
+                    parseBlock(blockLines, journalEntries);  // ToDo add parseBlock method
+                    blockLines.clear(); // reset for next block
+                }
+                else {
+                    blockLines.add(line); // collect lines
+            }
+
+        }
+            if (!blockLines.isEmpty()) { // at the end of the file - no separator
+                parseBlock(blockLines,journalEntries);
+            }
+    }
+        catch (IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+        }
+        return journalEntries;
+
+
 }
